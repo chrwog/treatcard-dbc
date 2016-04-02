e3b3e9de278fc43fae6c7cde0e0450cd838f71c6
chrwog <wongachris@gmail.com>
2016-04-02 15:47:07 -0700

    Initial Commit

---
+++ treatcard-dbc/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  treatcard-dbc
+//
+//  Created by Chris Wong on 4/2/16.
+//  Copyright © 2016 treatcard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ treatcard-dbcTests/treatcard_dbcTests.swift
@@ -0,0 +1,36 @@
+//
+//  treatcard_dbcTests.swift
+//  treatcard-dbcTests
+//
+//  Created by Chris Wong on 4/2/16.
+//  Copyright © 2016 treatcard. All rights reserved.
+//
+
+import XCTest
+@testable import treatcard_dbc
+
+class treatcard_dbcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
