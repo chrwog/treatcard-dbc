e57856e2eeed931250117fa6d9cb42e1e6602337
chrwog <wongachris@gmail.com>
2016-04-02 17:20:15 -0700

    Add NavBar, Icons, 1st Navigation controllers.

--- treatcard-dbc/SettingsViewController.swift
+++ treatcard-dbc/SettingsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  SettingsViewController.swift
 //  treatcard-dbc
 //
 //  Created by Chris Wong on 4/2/16.
@@ -8,24 +8,17 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SettingsViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
-//This is a change
-//asdlfsdf
-//This is wong work
-//here is shahin change
-
-//another change
