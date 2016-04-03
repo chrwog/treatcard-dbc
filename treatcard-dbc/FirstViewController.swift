af75a93da3b8616a01d11ef1d1e1405a69207faf
Chris <shaneenterprises@gmail.com>
2016-04-02 17:35:10 -0700

    Revert "San fontsandsplashscreen"

--- treatcard-dbc/FirstViewController.swift
+++ treatcard-dbc/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ReceivedViewController.swift
+//  FirstViewController.swift
 //  treatcard-dbc
 //
 //  Created by Chris Wong on 4/2/16.
@@ -8,17 +8,18 @@
 
 import UIKit
 
-class ReceivedViewController: UIViewController {
-    
+class FirstViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+
 }
+
